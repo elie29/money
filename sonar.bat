cf7b32a3db7bc1c97015c64cf58e47f19ae8a6ad
Elie NEHME <elie29@gmail.com>
2017-09-04 12:59:24 +0200

    indentation

--- sonar.bat
+++ sonar.bat
@@ -1,1 +1,3 @@
-sonar-scanner -Dsonar.login=aadd191f832e86431b209e401cd3e837a11ba904
+::sonar.login is added in sonar-scanner/conf/sonar-scanner.properties
+::sonar-scanner -Dsonar.login=aadd191f832e86431b209e401cd3e837a11ba904
+sonar-scanner
